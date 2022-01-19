f0c5d7ecd437c3515e9b0824b648fc66d3234fc0
Odinokun <odinokun@gmail.com>
2022-01-19 22:55:43 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  // begin SVG for IE
+  require('./modules/svg4everybody')();
+
+  // begin phone mask
+  // require('./modules/maskedinput')();
+
+  // begin Scroll2Ancor
+  // require('./modules/scroll2ancor')();
+
+  // begin Slick slider
+  // require('./modules/slick')();
+
+  // begin Popup
+  // require('./modules/popup')();
+
+  // begin Animate.css
+  //РАБОТАЕТ ТОЛЬКО С JQUERY < 3.0.0 !!!!!!!!!!!!!!!!!!
+  // require('./modules/waypoints')();
+  // require('./modules/animateCss')();
+
+});
