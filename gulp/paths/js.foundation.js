050cb352b826fcc40088480f800381be6ffb835f
Odinokun <odinokun@gmail.com>
2022-01-21 13:48:43 +0200

    did video section

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,7 +2,7 @@
 
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
-  './node_modules/svg4everybody/dist/svg4everybody.min.js'
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
 
   // include jQuery UI
   // './src/vendors/jquery-ui/jquery-ui.min.js',
@@ -10,9 +10,5 @@ module.exports = [
   // include Maskedinput
   // './src/vendors/maskedinput/jquery.maskedinput.min.js',
 
-  // include Slick slider
-  // './node_modules/slick-carousel/slick/slick.min.js',
-
-  // include fancybox3
-  // './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+  './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js'
 ];
